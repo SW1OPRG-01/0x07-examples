2126090efa91d1dfbf7f0efe2258f6ff844643a8
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2024-10-21 20:56:18 +0200

    Add registration

--- src/car/main.cpp
+++ src/car/main.cpp
@@ -8,10 +8,11 @@ int main(int argc, char *argv[]) {
   Car mercedes = Car("Mercedes", "EQB", 2024);
   Car bmw = Car("BMW", "i4 M50", 2023);
   Car volkswagen = Car("Volkswagen", "Golf Variant", 2015);
+  volkswagen.setRegistration("BT52590");
 
   cout << mercedes.getName() << endl;
   cout << bmw.getName() << endl;
-  cout << volkswagen.getName() << endl;
+  cout << volkswagen.getName() << " " << volkswagen.getRegistration() << endl;
 
   return 0;
 }
